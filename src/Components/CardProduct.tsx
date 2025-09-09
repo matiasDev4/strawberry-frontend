1583693a6e2e080e4a0872f79bb06bb2b5169bc8
matiasdev4 <matiasalvarezoaks@gmail.com>
2025-09-09 10:30:14 -0300

    update 3

--- src/Components/CardProduct.tsx
+++ src/Components/CardProduct.tsx
@@ -8,15 +8,15 @@ type productCardProp = {
 
 export const Card = ({product}:{product: productCardProp}) => {
     return (
-        <article className=" w-[300px] h-auto py-2 px-2 rounded-xl
+        <article className=" w-[320px] h-auto py-2 px-2 rounded-xl
          shadow-[0px_0px_12px_black]/15 border border-[#e2e2e2] bg-white">
             <div className='p-2 border rounded-lg border-[#d1d1d1] my-2'>
                 <img src={product.image}
                     className='w-full h-full object-cover rounded-lg'
                 />
             </div>
-            <div className='flex justify-between p-2 items-center'>
-                <h1 className='text-lg font-play'>{product.name}</h1>
+            <div className='flex flex-col gap-y-1 justify-between p-2 items-center'>
+                <h1 className='text-lg font-play'>{product.name.toUpperCase()}</h1>
                 <span className='text-md font-semibold bg-red-400 px-2 py-0.2 rounded-sm
                 text-white'>$ {product.price}</span>
             </div>
--- src/Components/Inicio.tsx
+++ src/Components/Inicio.tsx
@@ -5,17 +5,18 @@ export const Inicio = () => {
         <section 
         id='inicio'
         className='flex flex-col-reverse w-auto items-center lg:items-center lg:flex-row lg:justify-around my-8 lg:max-w-[1000px] lg:mx-auto scroll-mt-20'>
-            <div className='w-auto h-80 flex flex-col gap-y-5 justify-center p-3 my-5 lg:w-110'>
+            <div className='w-auto h-80 flex flex-col gap-y-3 justify-center p-3 my-5 lg:w-110'>
                 <h1 className='font-bold text-xl lg:text-3xl font-soft'>Bienvenida a <span className='
                 text-red-500 font-prise'>Strawberry!</span> 🍓</h1>
                 <p className='text-[#4d4d4d] font-normal font-play leading-relaxed text-md lg:text-lg'>
-                    Traemos ropa por encargue todos los sábados, 
+                    Traemos ropa por encargue todos los Lunes, 
                     te dejamos nuestro catálogo para que veas todo lo que tenemos disponible
                 </p>
-                {/* <button className='bg-red-500 py-2 w-full my-2 rounded-md font-normal text-white font-play
-                hover:cursor-pointer hover:bg-red-400 transition-colors duration-200'>
-                    Ver catalogo
-                </button> */}
+                <h2 className='font-play'>Podes unirte a nuestro grupo!</h2>
+                <a href='https://chat.whatsapp.com/Gx41AUfuOSv0Nu7EuT6eUj?mode=ems_copy_t' className='bg-green-500 py-2 w-full my-1 rounded-md font-normal text-white font-play
+                hover:cursor-pointer hover:bg-green-600 transition-colors duration-200 text-center'>
+                    Grupo de Whatsapp
+                </a> 
             </div>
             <div className='p-2 border-3 rounded-lg border-[#ffb6b6] w-60 h-60 lg:w-80 lg:h-80'>
                 <img src={logo} alt="logo"
