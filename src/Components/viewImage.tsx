5a7a8ed98b20da391fcec5982eac19a070d10791
matiasdev4 <matiasalvarezoaks@gmail.com>
2025-09-10 12:02:12 -0300

    update card product

--- src/Components/viewImage.tsx
+++ src/Components/viewImage.tsx
@@ -6,13 +6,13 @@ export const ViewImage = ({close}:{close: React.Dispatch<React.SetStateAction<bo
 
     return (
         <section className="fixed top-0 left-0 right-0 bottom-0 backdrop-blur-sm bg-black/50 flex items-center justify-center">
-            <div className="p-2">
+            <div className="px-4">
                 <img src={product?.image} alt={product?.name}
-                className="w-70 h-80 rounded-xl"/>
+                className="w-full h-full rounded-xl"/>
             </div>
             <button 
             onClick={()=>{close(false)}}
-            className="absolute top-10 right-5 text-xl bg-[#585858]/50 text-white p-2 rounded-full
+            className="absolute top-15 right-5 text-2xl bg-[#585858]/50 text-white p-2 rounded-full
             active:bg-red-500"><RiCloseLargeFill /></button>
         </section>
     )
